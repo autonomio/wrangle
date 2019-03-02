bb6ccc96deb9a00879a32bdc9d43a57b29b3682a
Name <39023769+C0NTRIBUTE@users.noreply.github.com>
2019-03-02 12:33:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ if __name__ == "__main__":
           install_requires=install_requires,
           packages=['wrangle',
                     'wrangle.utils',
+                    'wrangle.dic',
                     'wrangle.df',
                     'wrangle.col',
                     'wrangle.array'],
