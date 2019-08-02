a4b72bd4723f32a74545bddd274f7d0ae3d1eb78
Mikko Kotila <mailme@mikkokotila.com>
2019-08-02 15:52:57 +0300

    feature improvement to col_to_multilabel
    This commit also changes the version to 0.6.6 to sync all the recent updates with production/pypi.

--- wrangle/col/col_to_multilabel.py
+++ wrangle/col/col_to_multilabel.py
@@ -1,7 +1,11 @@
 import pandas as pd
 
 
-def col_to_multilabel(data, col, colnames=None, extended_colname=False):
+def col_to_multilabel(data,
+                      col,
+                      colnames=None,
+                      extended_colname=False,
+                      extended_separator='_'):
 
     '''Takes in a dataframe column with multiple labels and converts it
     into several columns with binary labels.
@@ -15,6 +19,8 @@ def col_to_multilabel(data, col, colnames=None, extended_colname=False):
     extended_colname : bool
         If True, then the original column name will be a prefix for the
         new column names.
+    extended_separator : str
+        The separator to be used in case `extended_colname` is in play.
     '''
 
     data = data.copy(deep=True)
@@ -26,6 +32,6 @@ def col_to_multilabel(data, col, colnames=None, extended_colname=False):
         temp_cols.columns = colnames
 
     if extended_colname is True:
-        temp_cols.columns = [col + '_' + i for i in temp_cols.columns]
+        temp_cols.columns = [col + extended_separator + str(i) for i in temp_cols.columns]
 
     return pd.merge(data, temp_cols, left_index=True, right_index=True)
